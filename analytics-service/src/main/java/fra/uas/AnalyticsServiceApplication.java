1cf53f286c0b6e42ba94ac0cbc3c06008f1f8847
andi-l <andi.latifi@stud.fra-uas.de>
2025-01-07 20:33:44 +0100

    Created analytics-service

---
+++ analytics-service/src/main/java/fra/uas/AnalyticsServiceApplication.java
@@ -0,0 +1,13 @@
+package fra.uas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyticsServiceApplication.class, args);
+    }
+
+}
