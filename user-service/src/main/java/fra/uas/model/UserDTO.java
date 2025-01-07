d9aaf4f81d458433aa8dc6c21b8e89e9fe4e02f0
andi-l <andi.latifi@stud.fra-uas.de>
2025-01-07 19:08:42 +0100

    Added UserDTO

---
+++ user-service/src/main/java/fra/uas/model/UserDTO.java
@@ -0,0 +1,35 @@
+package fra.uas.model;
+
+public class UserDTO {
+    private String username;
+    private String password;
+
+    public UserDTO(String username) {
+        this.username = username;
+        this.password = "";
+    }
+
+    public UserDTO() {}
+
+    public UserDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
