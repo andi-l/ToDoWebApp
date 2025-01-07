e84850ecfe7dbe7262a3c668b1f96f78d69f8016
andi-l <andi.latifi@stud.fra-uas.de>
2025-01-07 19:49:15 +0100

    Added TaskList model

---
+++ todo-service/src/main/java/fra/uas/model/TaskList.java
@@ -0,0 +1,78 @@
+package fra.uas.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class TaskList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String username;
+
+    private LocalDate creationDate;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "task_list_id")
+    @JsonProperty("tasks")
+    private List<Task> tasks;
+
+
+    public TaskList() {
+    }
+
+    public TaskList(String username, LocalDate creationDate, String title, List<Task> tasks) {
+        this.username = username;
+        this.creationDate = creationDate;
+        this.title = title;
+        this.tasks = tasks;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
