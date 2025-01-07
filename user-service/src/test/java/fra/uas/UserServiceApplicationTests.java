c7b2a861be4560fb591af3e70ca147ce88525190
andi-l <andi.latifi@stud.fra-uas.de>
2025-01-07 21:39:57 +0100

    Added all ApplicationTest classes and application.properties

---
+++ user-service/src/test/java/fra/uas/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fra.uas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class fUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
