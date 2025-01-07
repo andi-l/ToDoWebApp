b193257bda1046441cdbfb0f153f5ac5b838b4c7
andi-l <andi.latifi@stud.fra-uas.de>
2025-01-07 19:26:55 +0100

    Added login and protected in UserController

--- user-service/src/main/java/fra/uas/controller/UserController.java
+++ user-service/src/main/java/fra/uas/controller/UserController.java
@@ -1,13 +1,17 @@
 package fra.uas.controller;
 
 import fra.uas.model.User;
+import fra.uas.model.UserDTO;
 import fra.uas.service.TokenService;
 import fra.uas.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Controller
 public class UserController {
@@ -28,5 +32,25 @@ public class UserController {
         return ResponseEntity.ok(
                 "User " + user.getUsername() + " created successfully"
         );
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody UserDTO user) {
+        if (userService.validateUser(user.getUsername(), user.getPassword())) {
+            String token = tokenService.createToken(user.getUsername());
+            return ResponseEntity.ok("Token: " + token);
+        } else {
+            return ResponseEntity.badRequest().body("Invalid credentials");
+        }
+    }
+
+    // Protected endpoint
+    @GetMapping("/protected")
+    public ResponseEntity<?> protectedEndpoint(@RequestHeader("Authorization") String authToken) {
+        if (tokenService.isTokenValid(authToken)) {
+            return ResponseEntity.ok("Access to protected resource granted");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("You are not logged in or the session has expired");
+        }
     }
 }
